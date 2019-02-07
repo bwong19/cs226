896e06367318c206aa859fe59928ae48468c4a42
Brandon Wong <bwong19@jhu.edu>
2019-02-07 03:05:18 -0500

    finished hw1 part 1

--- homework/hw1/Unique.java
+++ homework/hw1/Unique.java
@@ -1,4 +1,6 @@
-/* TODO - Add your name, JHED, and email.
+/* Name: Brandon
+ * JHED: bwong19
+ * Email: bwong19@jhu.edu
  * Unique.java
  */
 
@@ -7,7 +9,7 @@ package hw1;
 /** A class with a main method for printing out unique numbers. */
 public final class Unique {
 
-    // Make checkstyle happy.
+    /** Make checkstyle happy. */
     private Unique() {
         throw new AssertionError("Can not instantiate class Unique\n");
     }
@@ -15,8 +17,48 @@ public final class Unique {
     /**
      * A main method to print the unique numerical command line arguments.
      * @param args The string array of arguments in the command line.
+     * @throws IllegalArgumentException if command line argument is invalid.
      */
-    public static void main(String[] args) {
-        // TODO
+    public static void main(String[] args) throws IllegalArgumentException {
+        // Limits upper bound on number of command line arguments
+        final int maxArgs = 255;
+        
+        if (args.length > maxArgs) {
+            throw new IllegalArgumentException("Too many arguments\n");
+        }
+
+        // Contains unique numbers.
+        int[] uniq = new int[args.length];
+        // Tracks size of uniq.
+        int uniqSize = 0;
+        
+        for (int i = 0; i < args.length; ++i) {   
+            // Checks if command line argument is an integer.
+            int tmp;
+            try {
+                tmp = Integer.parseInt(args[i]);
+            }
+            catch (NumberFormatException e) {
+                throw new IllegalArgumentException("Not an integer\n");
+            }
+            
+            // Checks if args[i] has been seen before
+            boolean isUnique = true;
+            for (int j = 0; j < uniqSize; ++j) {
+                if (tmp == uniq[j]) {
+                    isUnique = false;
+                    break;
+                }
+            }
+            // If args[i] has not been seen, add it to uniq
+            if (isUnique) {
+                uniq[uniqSize] = tmp;
+                uniqSize++;
+            }
+        }
+
+        for (int i = 0; i < uniqSize; ++i) {
+            System.out.println(uniq[i]);
+        }
     }
 }
