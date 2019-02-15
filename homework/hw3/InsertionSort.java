25b623ce0e3bd8d787516b9f04e7090d323d1264
Brandon Wong <bwong19@jhu.edu>
2019-02-15 12:27:08 -0500

    hw3 skeleton files

---
+++ homework/hw3/InsertionSort.java
@@ -0,0 +1,25 @@
+/* InsertionSort.java */
+
+package hw3;
+
+import hw2.Array;
+
+
+/**
+ * The Insertion Sort algorithm.
+ * @param <T> Element type.
+ */
+public final class InsertionSort<T extends Comparable<T>>
+    implements SortingAlgorithm<T> {
+
+
+    @Override
+    public void sort(Array<T> array) {
+        // TODO
+    }
+
+    @Override
+    public String name() {
+        return "Insertion Sort";
+    }
+}
